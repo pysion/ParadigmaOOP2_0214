bf15c0686abea5907a64c2dba4a5367c88c4ff33
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-06-02 18:42:18 +0700

    instalasi nama saklar di setaip ruangan

--- Enkapsulasi.cpp
+++ Enkapsulasi.cpp
@@ -17,5 +17,10 @@ class remotelampu {
 int main() {
     remotelampu lampuRumah;
 
+    lampuRumah.setSaklarNo (0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo (1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo (2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo (3, "Lampu Dapur");
+
    
 }
---
+++ NoPewarisan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+    public:
+        virtual void perkenalan() {
+            cout << "Hallo saya function dari base class";
+        }
+};
+
+class derivedClass : public baseClass {
+    public:
+}
