7aec837a527281b52d431c135e860ebc3b6a7ee7
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-06-02 18:53:18 +0700

    memmbuat class deruved class

--- NoOverriding.cpp
+++ NoOverriding.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 class baseClass {
     public:
-        virtual void perkenalan() final {
+        virtual void perkenalan() final{
             cout << "Hallo saya function dari base class";
         }
 };
+
+class derivedClass : public baseClass {
+    public:
+        void perkenalan() {
+            cout << "Hallo saya function dari derived Class";
+        }
+};
+
