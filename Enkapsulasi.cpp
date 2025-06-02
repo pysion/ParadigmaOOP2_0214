d02c04d51ca9ef32eab95320bf7d1c73cfc39775
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-06-02 18:33:42 +0700

    membuat void setsaklar no pada class publik remote lampu

--- Enkapsulasi.cpp
+++ Enkapsulasi.cpp
@@ -6,5 +6,10 @@ class remotelampu {
         string saklarNo[10];
 
     public:
-        
+        void setSaklarNo(int i, string value) {
+            saklarNo[i] = value;
+        }
+        string getSaklarNo(int i) {
+            return saklarNo[i];
+        }
 };
